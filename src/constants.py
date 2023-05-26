abc7a95aaea90b899ef5598d9e8af90c98a41a57
Patrick Huang <huangpatrick16777216@gmail.com>
2023-05-25 22:15:03 -0700

    Larger model.

--- src/constants.py
+++ src/constants.py
@@ -7,18 +7,18 @@ import torch
 # Sine embeddings
 FREQ_MIN = 1
 FREQ_MAX = 256
-EMBED_DIM = 8
+EMBED_DIM = 32
 # NeRF MLP model
-MLP_DIM = 64
-MLP_DEPTH = 4
+MLP_DIM = 256
+MLP_DEPTH = 16
 # Rendering
 CLIPPING = 4
-RENDER_STEPS = 16
+RENDER_STEPS = 32
 
 # Training parameters
 BATCH_SIZE = 64
 BATCH_PER_STEP = 1
-EPOCHS = 1
+EPOCHS = 5
 SAVE_INTERVAL = 1
-LR_START = 1e-3
-LR_END = 1e-5
+LR_START = 1e-2
+LR_END = 1e-4
